2102fadbb88df7a48ecce352761eb699b95f7979
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:03:15 +0300

    develop. Exp done.

--- src/Functions/s21_exp.c
+++ src/Functions/s21_exp.c
@@ -1,5 +1,17 @@
 #include "s21_math.h"
-// TODO Implement s21_exp
+/**
+ * @brief Returns e raised to the given power.
+ *
+ * @param x Power.
+ * @return Raised e.
+ * @retval long double
+ */
 long double s21_exp(double x) {
-    return x;
+  if (x == S21_INF_NEG) return 0;
+  long double result = 0;
+  long double k = (long long int)(x / S21_LN2);
+  long double r = x - k * S21_LN2;
+  for (int i = 0; i < 128; i++) result += s21_pow_int(r, i) / s21_fuctorial(i);
+  result *= s21_pow_int(2, k);
+  return result;
 }
