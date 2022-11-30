0c750baef93078f74b3f673c1345844bdfca3445
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:04:54 +0300

    develop. Pow done.

--- src/Functions/s21_pow.c
+++ src/Functions/s21_pow.c
@@ -1,5 +1,24 @@
 #include "s21_math.h"
-// TODO Implement s21_pow
+/**
+ * @brief Raises a number to the given power.
+ *
+ * @param base Number to raise.
+ * @param exp Power.
+ * @return Raised number.
+ * @retval long double
+ */
 long double s21_pow(double base, double exp) {
-    return base * exp;
+  if (!exp) return 1;
+  if (base == S21_INF_NEG && exp == S21_INF_NEG) return 0;
+  if (base == S21_INF_NEG && exp == S21_INF_POS) return S21_INF_POS;
+  if (base < 0 && exp != (long long int)exp) return S21_NAN;
+  long double res = 0;
+  if (base >= 0) {
+    res = s21_exp(exp * s21_log(base));
+  } else {
+    base = -base;
+    res = s21_exp(exp * s21_log(base));
+    if (s21_fmod(exp, 2)) res = -res;
+  }
+  return res;
 }
--- src/Functions/s21_pow_int.c
+++ src/Functions/s21_pow_int.c
@@ -1,5 +1,17 @@
 #include "s21_math.h"
-// TODO Implement s21_pow_int
+/**
+ * @brief Raises a number to the given power.
+ *
+ * @param base Number to raise.
+ * @param exp Power.
+ * @return Raised number.
+ * @retval long double
+ */
 long double s21_pow_int(double base, long long int exp) {
-    return base * exp;
+  long double result = 1;
+  if (exp > 0)
+    for (long long int i = 0; i < exp; i++) result *= base;
+  else
+    for (long long int i = 0; i < -exp; i++) result /= base;
+  return result;
 }
