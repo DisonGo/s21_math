06924d51d00a394f322b7d42b922dea444689700
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:03:30 +0300

    develop. Fabs done.

--- src/Functions/s21_fabs.c
+++ src/Functions/s21_fabs.c
@@ -1,5 +1,12 @@
 #include "s21_math.h"
-// TODO Implement s21_fabs
+/**
+ * @brief Get absolute value of x.
+ *
+ * @param x Input value.
+ * @return Absolute value of x.
+ * @retval long double
+ */
 long double s21_fabs(double x) {
-    return x;
+  if (x < 0) x = -x;
+  return (long double)x;
 }
