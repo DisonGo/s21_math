0869834e789dac862a385596daaeaf9250ad49dd
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:03:51 +0300

    develop. Floor done.

--- src/Functions/s21_floor.c
+++ src/Functions/s21_floor.c
@@ -1,5 +1,14 @@
 #include "s21_math.h"
-// TODO Implement s21_floor
+/**
+ * @brief Returns the nearest integer not greater than x.
+ *
+ * @param x Input value.
+ * @return Nearest integer not greater than x.
+ * @retval long double
+ */
 long double s21_floor(double x) {
-    return x;
+  if (x != x || x == S21_INF_POS || x == S21_INF_NEG) return x;
+  int int_part = (int)x;
+  int_part -= (x < 0 && (x - int_part));
+  return (long double)int_part;
 }
