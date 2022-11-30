e8c873301130e158a27ec3ec6c7ab0957a5430b8
evverenn <evverenn@student.21-school.ru>
2022-11-30 14:28:21 +0300

    develop. Fuctorial done.

--- src/Functions/s21_fuctorial.c
+++ src/Functions/s21_fuctorial.c
@@ -1,3 +1,14 @@
 #include "s21_math.h"
-// TODO Implement s21_fuctorial
-long double s21_fuctorial(long long int x) { return x; }
+/**
+ * @brief Computes factorial of x.
+ * 
+ * @param x Input value.
+ * @return Factorial of x.
+ * @retval long double 
+ */
+long double s21_fuctorial(long long int x) {
+  if (x == 1 || x == 0) return 1;
+  if (x < 0) return S21_INF_POS;
+  long double res = x * s21_fuctorial(x - 1);
+  return res;
+}
