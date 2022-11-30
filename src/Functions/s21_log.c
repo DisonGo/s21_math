97459fe0cd3889f846e0896252a33e7fa78558bc
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:04:29 +0300

    develop. Log done.

--- src/Functions/s21_log.c
+++ src/Functions/s21_log.c
@@ -1,5 +1,22 @@
 #include "s21_math.h"
-// TODO Implement s21_log
+/**
+ * @brief Computes natural logarithm.
+ *
+ * @param x Input value.
+ * @return Natural logarithm.
+ * @retval long double
+ */
 long double s21_log(double x) {
-    return x;
+  if (x == S21_INF_POS) return x;
+  if (x == 0) return S21_INF_NEG;
+  if (x < 0) return S21_NAN;
+  long double result = 0, compare = 0;
+  int e_repeat = 0;
+  for (; x >= S21_E; e_repeat++) x /= S21_E;
+  for (int i = 0; i < 100; i++) {
+    compare = result;
+    result = compare + 2 * (x - s21_exp(compare)) / (x + s21_exp(compare));
+  }
+  result += e_repeat;
+  return result;
 }
