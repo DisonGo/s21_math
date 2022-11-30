c79677ed687f6c601b5b802f7a9bae3830f57324
evverenn <evverenn@student.21-school.ru>
2022-11-30 13:05:13 +0300

    develop. Sqrt done.

--- src/Functions/s21_sqrt.c
+++ src/Functions/s21_sqrt.c
@@ -1,5 +1,21 @@
 #include "s21_math.h"
-// TODO Implement s21_sqrt
+/**
+ * @brief Computes square root.
+ *
+ * @param x Input value.
+ * @return Square root of x.
+ * @retval long double
+ */
 long double s21_sqrt(double x) {
-    return x;
+  if (x != x || x == S21_INF_NEG || x < 0) return S21_NAN;
+  if (x == S21_INF_POS) return (long double)x;
+  long double mantissa = 0, num = -1;
+  while (s21_pow((num + 2), 2) <= x) num++;
+  for (int j = 0; j <= 12; j++) {
+    long double del = 1.0 / s21_pow_int(10, j);
+    for (int i = 0; s21_pow((num + (i + 1) * del), 2) <= x; i++)
+      mantissa = i * del;
+    num += mantissa;
+  }
+  return num;
 }
