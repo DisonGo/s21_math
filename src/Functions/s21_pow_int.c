3789c35b56b2ff120cc054e4501114c450c4c701
evverenn <evverenn@student.21-school.ru>
2022-11-30 16:23:54 +0300

    develop. Pow_int refactor.

--- src/Functions/s21_pow_int.c
+++ src/Functions/s21_pow_int.c
@@ -9,9 +9,9 @@
  */
 long double s21_pow_int(double base, long long int exp) {
   long double result = 1;
-  if (exp > 0)
-    for (long long int i = 0; i < exp; i++) result *= base;
-  else
-    for (long long int i = 0; i < -exp; i++) result /= base;
+  int sign = (exp > 0);
+  exp *= sign ? 1 : -1;
+  for (long long int i = 0; i < exp; i++)
+    result = sign ? result * base : result / base;
   return result;
 }
